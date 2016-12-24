3b99f0cb319de69286ae8b452fa6938dedb6ab2d
audahadi <wan.audahadi@gmail.com>
2016-12-24 15:53:14 +0800

    re-structure and commonize blob
    Change-Id: I0dfc353a4b16b643fb47fa22879f9e9766675bc6

--- Z010D/proprietary/etc/ins_module.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-# ASUS_BSP +++ Gavin_Chang "ntfs and exfat support"
-#insmod /system/lib/modules/tntfs.ko
-insmod /system/lib/modules/texfat.ko
-# ASUS_BSP --- Gavin_Chang "ntfs and exfat support"
