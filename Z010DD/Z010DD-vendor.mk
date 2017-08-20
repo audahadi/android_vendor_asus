550ae6595d8e1b6754c7f5578689107978ef223a
audahadi <wan.audahadi@gmail.com>
2017-08-20 19:12:43 +0800

    add Z010DD firmware

---
+++ Z010DD/Z010DD-vendor.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pick up overlay for features that depend on non-open-source files
+
+PRODUCT_COPY_FILES += \
+    vendor/asus/Z010DD/proprietary/etc/firmware/keymaster.b00:install/bin/firmware/keymaster.b00 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/keymaster.b01:install/bin/firmware/keymaster.b01 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/keymaster.b02:install/bin/firmware/keymaster.b02 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/keymaster.b03:install/bin/firmware/keymaster.b03 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/keymaster.mdt:install/bin/firmware/keymaster.mdt \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b00:install/bin/firmware/wcnss.b00 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b01:install/bin/firmware/wcnss.b01 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b02:install/bin/firmware/wcnss.b02 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b04:install/bin/firmware/wcnss.b04 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b06:install/bin/firmware/wcnss.b06 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b09:install/bin/firmware/wcnss.b09 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b10:install/bin/firmware/wcnss.b10 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.b11:install/bin/firmware/wcnss.b11 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/wcnss.mdt:install/bin/firmware/wcnss.mdt \
+    vendor/asus/Z010DD/proprietary/etc/firmware/cmnlib.b00:install/bin/firmware/cmnlib.b00 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/cmnlib.b01:install/bin/firmware/cmnlib.b01 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/cmnlib.b02:install/bin/firmware/cmnlib.b02 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/cmnlib.b03:install/bin/firmware/cmnlib.b03 \
+    vendor/asus/Z010DD/proprietary/etc/firmware/cmnlib.mdt:install/bin/firmware/cmnlib.mdt 
+
